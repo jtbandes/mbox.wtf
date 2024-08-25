c0aec8c7a217923d468ca546c65a67911ce9406a
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2024-08-25 16:22:31 -0700

    set up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import eslint from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import tseslint from "typescript-eslint";
+
+/**
+ * Workaround for https://github.com/sveltejs/eslint-plugin-svelte/pull/789#issuecomment-2309033145
+ *
+ * @type {{
+ *    [K in keyof typeof eslintPluginSvelte["configs"]]:
+ *      typeof eslintPluginSvelte["configs"][K] extends unknown[]
+ *        ? import("eslint").Linter.Config[]
+ *        : import("eslint").Linter.Config
+ * }} */
+const eslintPluginSvelteConfigs = eslintPluginSvelte.configs;
+
+/**
+ * Note: must be consistent per tsconfig project: https://github.com/typescript-eslint/typescript-eslint/issues/6778#issuecomment-1484725834
+ * @type {import("@typescript-eslint/parser").ParserOptions}
+ */
+const parserOptions = {
+  projectService: {
+    allowDefaultProject: ["*.js", "*.mjs"],
+    defaultProject: "tsconfig.default.json",
+  },
+  tsconfigRootDir: import.meta.dirname,
+  extraFileExtensions: [".svelte"],
+};
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...eslintPluginSvelteConfigs["flat/recommended"],
+  ...eslintPluginSvelteConfigs["flat/prettier"],
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions,
+    },
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+  {
+    files: ["**/*.svelte", "*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+        ...parserOptions,
+      },
+    },
+  },
+);
