2172c90232525a5b1d8989bec9a86290d967b1f7
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2024-09-29 17:05:51 -0700

    workaround for https://bugs.webkit.org/show_bug.cgi?id=280593

--- src/readLines.ts
+++ src/readLines.ts
@@ -5,7 +5,8 @@ export type ReadLinesValue = {
 
 const CHUNK_SIZE = 2 * 1024 * 1024; // 2MB
 export async function* readLines(file: File): AsyncGenerator<ReadLinesValue> {
-  const decoder = new TextDecoder();
+  let decoder = new TextDecoder();
+  let bytesSinceNewDecoder = 0;
   let str = "";
   for (let fileOffset = 0; fileOffset < file.size; fileOffset += CHUNK_SIZE) {
     const done = fileOffset + CHUNK_SIZE >= file.size;
@@ -15,6 +16,20 @@ export async function* readLines(file: File): AsyncGenerator<ReadLinesValue> {
     // https://bugs.webkit.org/show_bug.cgi?id=272600
     const chunk = await file.slice(fileOffset, fileOffset + CHUNK_SIZE).arrayBuffer();
     str += decoder.decode(chunk, { stream: !done });
+
+    // Workaround for Safari TextDecoder crashing after >2GB. Lazily assuming
+    // we'll eventually hit a chunk that ends with an ASCII byte
+    // https://bugs.webkit.org/show_bug.cgi?id=280593
+    bytesSinceNewDecoder += chunk.byteLength;
+    if (bytesSinceNewDecoder > 1024 * 1024 * 1024) {
+      const u8 = new Uint8Array(chunk);
+      const lastByte = u8[u8.length - 1];
+      if (lastByte == undefined || (lastByte & 0x80) === 0) {
+        // Ended on an ASCII byte, safe to reset decoder
+        decoder = new TextDecoder();
+        bytesSinceNewDecoder = 0;
+      }
+    }
 
     let offset = 0;
     let newlineOffset;
